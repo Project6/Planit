46c57166f405f0fd39ef81d6c9fb6b96f1ad682c
dglambert <dglambert@outlook.com>
2014-03-10 23:37:04 -0400

    added Planit.CommandLine back, also worked out bugs and ran TraverseByDueDate inside of Planit.CommandLine, although don't have sufficient seed data to verify

---
+++ Planit/Planit.CommandLine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Planit.Core;
+
+namespace Planit.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProjectBusinessLayer BAL = new ProjectBusinessLayer(new TestProjectDataLayer());
+
+            IEnumerable<Project> list = BAL.TraverseByDueDate();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Description + "\n\t\t" + item.DueDate);
+            }
+
+        }
+    }
+}
