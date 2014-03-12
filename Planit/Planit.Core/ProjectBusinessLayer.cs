22d1a2cdf6c9a06cbc267a9413d3a7d26a5b08d6
dglambert <dglambert@outlook.com>
2014-03-11 22:27:24 -0400

    refactoring ProjectBusiness Layer

--- Planit/Planit.Core/ProjectBusinessLayer.cs
+++ Planit/Planit.Core/ProjectBusinessLayer.cs
@@ -8,65 +8,78 @@ namespace Planit.Core
 {
     public class ProjectBusinessLayer
     {
+        #region Fields & Properties
 
-        public IProjectDataLayer _DAL {get; private set;}
+            public IProjectDataLayer _DAL {get; private set;}
+            public IEnumerable<Project> _Tree { get; private set; }
+            public List<Project> _List { get; private set; }
+        
+        #endregion
 
-        public ProjectBusinessLayer(IProjectDataLayer DAL)
-        {
-            _DAL = DAL;
-        }
-
-        public IEnumerable<Project> DFS()
-        {
-            return DFS(_DAL.Root);
-        }
-
-        public IEnumerable<Project> DFS(Project parent)
-        {
-            foreach (var child in parent.Children)
+        #region Constructors
+            
+            public ProjectBusinessLayer(IProjectDataLayer DAL)
             {
-                yield return child;
-                foreach (var grandchild in DFS(child))
+                _DAL = DAL;
+                _Tree = DFS();
+                _List = _Tree.ToList<Project>();
+            }
+
+        #endregion 
+
+        #region Methods
+        
+            public IEnumerable<Project> DFS()
+            {
+                return DFS(_DAL.Root);
+            }
+
+            public IEnumerable<Project> DFS(Project parent)
+            {
+                foreach (var child in parent.Children)
                 {
-                    yield return grandchild;
+                    yield return child;
+                    foreach (var grandchild in DFS(child))
+                    {
+                        yield return grandchild;
+                    }
                 }
             }
-        }
 
-        public IEnumerable<Project> TraverseByDueDate()
-        {
-            return TraverseByDueDate(_DAL.Root);
-        }
+            public IEnumerable<Project> TraverseByDueDate()
+            {
+                return TraverseByDueDate(_DAL.Root);
+            }
 
-        public IEnumerable<Project> TraverseByDueDate(Project parent)
-        {
-            IEnumerable<Project> tree = DFS(parent);
+            public IEnumerable<Project> TraverseByDueDate(Project parent)
+            {
+                
 
-            List<Project> list = tree.ToList<Project>();
-
-            list.Sort( (first, second) => first.DueDate.CompareTo(second.DueDate) >= 0 ? 1 : -1);
+                list.Sort( (first, second) => first.DueDate.CompareTo(second.DueDate) >= 0 ? 1 : -1);
           
-            return list;
-        }
+                return list;
+            }
 
-        //public List<Project> GetProjects()
-        //{
-        //    return _repository.GetProjects();
-        //}
-        //
-        //public void AddChild(Project child, Project parent)
-        //{
-        //    parent.Children.Add(child);
-        //}
+            public Project Find(int id)
+            {
+                
+            }
 
-        //public void Remove(Project project)
-        //{ 
-        //    // remove
-        //} 
+            //public List<Project> GetProjects()
+            //{
+            //    return _repository.GetProjects();
+            //}
+            //
+            //public void AddChild(Project child, Project parent)
+            //{
+            //    parent.Children.Add(child);
+            //}
 
-        //public void Find(Project project)
-        //{ 
-            
-        //}
+            //public void Remove(Project project)
+            //{ 
+            //    // remove
+            //} 
+        
+        #endregion
     }
 }
