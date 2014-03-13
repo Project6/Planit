5dfde53f8f491d9159bd642474caa0a0767147fc
beth van belle <bethvanbelle@gmail.com>
2014-03-13 18:34:56 -0400

    added basic calendar to schedule view

--- Planit/Planit/App_Start/BundleConfig.cs
+++ Planit/Planit/App_Start/BundleConfig.cs
@@ -9,7 +9,11 @@ namespace Planit
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.min.js",
+                         "~/Scripts/moment.min.js",
+                          "~/Scripts/fullcalendar.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -27,7 +31,8 @@ namespace Planit
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/fullcalendar.css"));
         }
     }
 }
