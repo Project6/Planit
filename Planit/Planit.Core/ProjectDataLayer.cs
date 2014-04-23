e7b08a047b2be62cf5fe43770ea41a17c0d83477
Devin <dglambert@outlook.com>
2014-04-23 07:46:06 -0400

    removed unused usings in DAL and BAL to try and resolve azure deployment failure

--- Planit/Planit.Core/ProjectDataLayer.cs
+++ Planit/Planit.Core/ProjectDataLayer.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Data;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
 
 
 namespace Planit.Core
