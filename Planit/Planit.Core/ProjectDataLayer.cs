9d24763f9403814eb7fe25ec579b60b3da5ae2f2
Devin <dglambert@outlook.com>
2014-04-23 06:30:33 -0400

    removed extra comments in DAL

--- Planit/Planit.Core/ProjectDataLayer.cs
+++ Planit/Planit.Core/ProjectDataLayer.cs
@@ -16,9 +16,9 @@ namespace Planit.Core
         public Project Root { get; private set; }
         public ProjectDbContext db = new ProjectDbContext("ProjectDB");
        
-            public ProjectDataLayer(string Userid, string UserName)
+        public ProjectDataLayer(string Userid, string UserName)
         {
-           try
+            try
             {
                 Root = db.Projects
                                     .Where(p => p.Depth == 0)
@@ -37,16 +37,14 @@ namespace Planit.Core
                                         UserID = Userid};
                 Root = Add(Root);
             }
-            //SeedRoot();
-            //Seed();
         }
 
-            public IQueryable<Project> GetProjects()
-            {
-                var projects = from m in db.Projects
-                               select m;
-                return projects;
-            }
+        public IQueryable<Project> GetProjects()
+        {
+            var projects = from m in db.Projects
+                            select m;
+            return projects;
+        }
         
         public Project Add(Project project)
         {
@@ -70,17 +68,18 @@ namespace Planit.Core
             db.SaveChanges();
         }
 
-
         public void Update(Project project)
         {
             Project projectOld = Find(project.ID);
             db.Entry(projectOld).CurrentValues.SetValues(project);
             db.SaveChanges();
         }
+
         public Project Find(int? id)
         {
             return db.Projects.Find(id);
         }
+        
         public Project Remove(Project project)
         {
             Project removed = db.Projects.Remove(project);
@@ -263,38 +262,6 @@ namespace Planit.Core
         {
             Root = new Project() { Title = "Devin's Task's", Depth = 0, DueDate = new DateTime(2020, 01, 01), StartDate = new DateTime(2014, 3, 31) };
         }
-
-        //public List<Project> GetProjects()
-        //{
-        //    List<Project> projectList = new List<Project>();
-
-
-
-        //    return projectList;
-
-        //}
-
-        //projectList.Add(new Project(001, "9999", 1, "Chores", new DateTime(),new DateTime(2014, 12, 31)));
-        //projectList.Add(new Project(002, "9999", 1, "School", new DateTime(),new DateTime(2014, 5, 10)));
-        //projectList.Add(new Project(003, "9999", 1, "Work", new DateTime(),new DateTime(2014, 12, 31)));
-
-        ////Chores >...
-        //projectList.Add(new Project(004, "9999", 2, "Gardening", new DateTime(2014,04,01),new DateTime(2014, 5, 28)));
-        //projectList.Add(new Project(005, "9999", 2, "Bills", new DateTime(),new DateTime(2014, 12, 30)));
-
-        ////Gardening > ...
-        //projectList.Add(new Project(006, "9999", 3, "Gather Supplies", new DateTime(2014, 04,01),new DateTime(2014, 4, 10)));
-        //projectList.Add(new Project(007, "9999", 3, "Prep", new DateTime(2014, 04, 11),new DateTime(2014, 04, 20)));
-        //projectList.Add(new Project(008, "9999", 3, "Plant", new DateTime(2014, 04, 21),new DateTime(2014, 5, 01)));
-
-        //// School > ..
-        //projectList.Add(new Project(009, "9999", 2, "CSC201j", new DateTime(),new DateTime(2014, 5, 10)));
-        //projectList.Add(new Project(010, "9999", 2, "CSC202j", new DateTime(),new DateTime(2014, 5, 10)));
-
-        //// CSC201j > ..
-        //projectList.Add(new Project(011, "9999", 2, "Reading", new DateTime(),new DateTime(2014, 5, 10)));
-        //projectList.Add(new Project(012, "9999", 2, "Labs", new DateTime(),new DateTime(2014, 5, 10
-
         
     }
 }
